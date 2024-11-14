d187b3dd25fb7d37f7937243f9af29c477f71013
Chisom Ikechukwu <chisom.ikechukwu@kcl.ac.uk>
2024-11-14 15:35:12 +0000

    implement the api gateway service to work with the summarize service

--- api_gateway_service/main.py
+++ api_gateway_service/main.py
@@ -1,17 +1,45 @@
 from fastapi import FastAPI, HTTPException
 import requests
-from pydantic import BaseModel
+#from pydantic import BaseModel
+import logging
+import httpx
+#from typing import List, Optional
+from shared.models import summary_req
 
 app = FastAPI()
 
-class summaryRequest(BaseModel):
-    text: str
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+#this defines the 
+#class summaryRequest(BaseModel):
+ #   text: Optional[str] = None
+  #  texts: Optional[str] = None
+   # text: Optional[int] = None
+    #text: Optional[int] = None
+
 
 @app.post("/summarize/")
-def summarize(request: summaryRequest):
+async def summarize(request: summary_req):  #this handles the incoming request
+    logger.info(f"API request with text: {bool(request.text)} and texts: {bool(request.texts)}")
+
+    if not request.text and not request.texts:
+        logger.warning("Input is empty. no text has beeen provided")
+        raise HTTPException(status_code=400, detail="Text input cannot be empty.")
+    
     try:
-        response = requests.post("http://summarize_service:8001/summarize/", json=request.dict())
-        response.raise_for_status()
-        return response.json()
-    except requests.RequestException as e:
-        raise HTTPException(status_code=500, detail=str(e))
+       
+        logger.info("Request sent to the summarize service")
+        async with httpx.AsyncClient() as client:
+
+            response = await client.post("http://summarize_service:8001/summarize/", json=request.dict())
+            response.raise_for_status()
+            
+            return response.json()
+        
+    except httpx.RequestException as e:
+        logger.info(f"error in connecting to summarize_service: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Error connecting to summarization service: {str(e)}")
+    except Exception as e:
+        logger.info(f"error returned by summarize service: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
